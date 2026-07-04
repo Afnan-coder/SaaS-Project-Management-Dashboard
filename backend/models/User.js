6420940db24c1cdad5bac6709ecbb8ff36176fdc
Afnan-coder <muhammadafnanali2@gmail.com>
2026-07-04 16:12:56 +0500

    feat(update): Update User schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,9 +4,12 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, unique: true },
     password: { type: String, required: true },
-    role : {type: String, enum: ['super_admin', 'manager', 'developer', 'user'], default: 'user'},
+    role: { type: String, enum: ['super_admin', 'manager', 'developer', 'user'], default: 'user' },
     resetPasswordToken: { type: String, default: null },
     resetPasswordExpiry: { type: Date, default: null },
-})
+},
+    {
+        timestamps: true
+    });
 
 export default mongoose.model("User", userSchema)
