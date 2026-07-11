7fbc085c7196c6cbbef455b020880f0585e27396
Afnan-coder <muhammadafnanali2@gmail.com>
2026-07-11 12:15:37 +0500

    feat(dashboard): create dashboardController function to get stats

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -1,5 +1,39 @@
+import Project from "../models/Project.js";
+import Task from "../models/Task.js";
 
 // Get Dashboard Statistics
 export const getDashboardStats = async (req, res) => {
-    
+    try {
+
+        const totalProjects = await Project.countDocuments();
+
+        const activeProjects = await Project.countDocuments({
+            status: "Active",
+        });
+
+        const pendingTasks = await Task.countDocuments({
+            status: "Todo",
+        });
+
+        const completedTasks = await Task.countDocuments({
+            status: "Completed",
+        });
+
+        return res.status(200).json({
+            success: true,
+            data: {
+                totalProjects,
+                activeProjects,
+                pendingTasks,
+                completedTasks,
+            },
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Failed to fetch dashboard statistics",
+            error: error.message,
+        });
+    }
 };
