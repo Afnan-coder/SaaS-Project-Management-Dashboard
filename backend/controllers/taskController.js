c6775d2c526f6cd6e9291b6a6e2aad4e4c2c6cfd
Afnan-coder <muhammadafnanali2@gmail.com>
2026-07-11 11:36:16 +0500

    feat(task): create the delete task controller function

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -178,4 +178,27 @@ export const updateTask = async (req, res) => {
 // Delete a task
 export const deleteTask = async (req, res) => {
 
+    try {
+        
+        const task = await Task.findByIdAndDelete(req.params.id);
+
+        if(!task) {
+            return res.status(404).json({
+                success: false,
+                message: "Task not found"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Task deleted successfully",
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to delete task "+error.message,
+        });
+    }
+
 };
