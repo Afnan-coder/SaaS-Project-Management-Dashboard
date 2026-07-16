0f93828c4de012bfda761c058364f2be890a263a
Afnan-coder <muhammadafnanali2@gmail.com>
2026-07-16 17:52:04 +0500

    update(task): Update getAllTasks  controller to apply search and filter

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -48,15 +48,45 @@ export const getAllTasks = async (req, res) => {
 
         const page = Number(req.query.page) || 1;
         const limit = Number(req.query.limit) || 10;
-        const project = req.query.project;
+
+        const {
+            project,
+            status,
+            priority,
+            assignee,
+            search
+        } = req.query;
 
         const skip = (page - 1) * limit;
 
         let filter = {};
 
-        // If project id is provided, return only tasks of that project
+        // Filter by Project
         if (project) {
             filter.project = project;
+        }
+
+        // Filter by Status
+        if (status) {
+            filter.status = status;
+        }
+
+        // Filter by Priority
+        if (priority) {
+            filter.priority = priority;
+        }
+
+        // Filter by Assignee
+        if (assignee) {
+            filter.assignee = assignee;
+        }
+
+        // Search by Task Title
+        if (search) {
+            filter.title = {
+                $regex: search,
+                $options: "i",
+            };
         }
 
         const totalTasks = await Task.countDocuments(filter);
