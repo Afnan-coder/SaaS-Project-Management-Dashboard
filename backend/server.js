b498afa4e340341ef975c33bfa42a4fe65c3a0e7
Afnan-coder <muhammadafnanali2@gmail.com>
2026-07-17 10:35:38 +0500

    update(cors): update the cors to support the frontend url)

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,12 @@ import dashboardRoutes from './routes/dashboardRoutes.js'
 const app = express()
 dotenv.config()
 app.use(express.json())
-app.use(cors())
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser())
 connectDB()
 
